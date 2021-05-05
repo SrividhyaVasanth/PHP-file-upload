d0abb744826d6a2aa652db490c2eb34330895c6d
Srividhya <76178081+SrividhyaVasanth@users.noreply.github.com>
2021-05-05 16:24:44 +0530

    Update d1.php

--- d1.php
+++ d1.php
@@ -17,6 +17,23 @@ for($i=1;$i<=$rowcount;$i++)
 <td><a href="/uploads/<?php echo $row["file"] ?>"><?php echo $row["file"] ?></a></td>
 
 </tr>
+<?php
+if(!empty($_GET['file'])){   $Name_Of_File = basename($_GET['file']);
+  $Path_of_file = 'uploads/'.$Name_Of_File;
+    if(!empty($Name_Of_File) && file_exists($Path_of_file)){
+        // Define headers
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Disposition: attachment; filename=$Name_Of_File");
+        header("Content-Type: application/png");
+        header("Content-Transfer-Encoding: binary");
+        readfile($Path_of_file);                     //for reading the file
+        exit;
+    }else{
+        echo 'The file you want here does not exist.';
+    }
+}
+?>
 
 <?php   
 }
